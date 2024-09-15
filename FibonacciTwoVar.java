9ce4066418cc01824b6cb821d7a9d5e144f15b35
lindazhang2026 <lindazhang26@marlborough.org>
2024-09-15 15:16:30 -0700

    Create FibonacciTwoVar.java

---
+++ FibonacciTwoVar.java
@@ -0,0 +1,24 @@
+public class FibonacciTwoVar
+{
+    public static void main(String args[])
+    {
+        int count = 30;
+        int a = 1;
+        int b = 1;
+        
+        System.out.println("Fibonacci with only two variables");
+        System.out.println(a);
+        
+        if (count > 1)
+        {
+            System.out.println(b);
+        }
+        
+        for (int i = 2; i < count; i++)
+        {
+            a += b;
+            System.out.println(a);
+        }
+        
+    }
+}
