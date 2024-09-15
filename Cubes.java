91e418069b8322c226f83e52ca659d9d0ce64cef
lindazhang2026 <lindazhang26@marlborough.org>
2024-09-15 14:56:14 -0700

    Cubes

---
+++ Cubes.java
@@ -0,0 +1,14 @@
+public class Cubes
+{
+    public static void main(String args[])
+    {
+        System.out.println("Cubes");
+        System.out.println();
+        
+        for (int i=0; i<10; i++)
+        {
+            int cube = i * i * i;
+            System.out.println(cube);
+        }
+    }
+}
