942688c50377044527910a755bbebd776242ed1c
lindazhang2026 <lindazhang26@marlborough.org>
2024-09-15 15:04:59 -0700

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+public class Fibonacci
+{
+    public static void main(String args[])
+    {
+        int count = 30;
+        int a = 1;
+        int b = 1;
+        
+        System.out.println("Fibonacci Sequence");
+        System.out.println(a);
+      
+        if (count > 1)
+        {
+            System.out.println(b);
+        }
+        
+        for (int i = 1; i < count; i++)
+        {
+            int next = a + b;
+            System.out.println(next);
+            a=b;
+            b=next;
+        }
+    }
+           
+}
