75b1fc5b8e25c457ddd84894c9cd8b2d22ef780f
lindazhang2026 <lindazhang26@marlborough.org>
2024-09-15 14:37:20 -0700

    Create MultiplesOf3.java
    Multiples Of 3

---
+++ MultiplesOf3.java
@@ -0,0 +1,14 @@
+public class MultiplesOf3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Sequences");
+        System.out.println();
+        //print multiples of 3
+        for (int i=0; i<10; i++)
+        {
+            if (i%3==0)
+            System.out.println(i);
+        }
+    }
+}
