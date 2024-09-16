671544651af113671fc99afe995a87e1d289848e
lindazhang2026 <lindazhang26@marlborough.org>
2024-09-16 06:33:05 -0700

    Cubes2

---
+++ Cubes2.java
@@ -0,0 +1,15 @@
+public class Cubes2
+{
+    public static void main(String args[])
+    {
+        int count = 6;
+        int term = 0;
+        System.out.println("First 6 terms in a sequence of cubes:");
+        
+        for (int i=1; i<=count; i++)
+        {
+            int cube = i * i * i;
+            System.out.println(cube);
+        }
+    }
+}
